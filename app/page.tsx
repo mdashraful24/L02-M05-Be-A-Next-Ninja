8bb486e5acc17d9bf122307a64f62717fecc8b0a
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-07-14 02:59:04 +0600

    added explore linking and navigation also loading in nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       Hello NextJS
+
+      Blog Page <Link href="/blogs/1">Blogs</Link>
     </div>
   );
 }
