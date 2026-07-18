a1cab47d0b7c58b83d848df2eec85d9dcb401d4e
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-07-19 01:31:30 +0600

    updated types and style

--- nextjs-press-frontend-project/app/(authGroup)/_components/RegisterForm.tsx
+++ nextjs-press-frontend-project/app/(authGroup)/_components/RegisterForm.tsx
@@ -7,10 +7,10 @@ const RegisterForm = () => {
     return (
         <form className='space-y-4'>
             <Card className='p-5 space-y-4'>
-                <Input name='name' type='name' placeholder='Enter your full name' />
+                <Input name='name' type='text' placeholder='Enter your full name' />
                 <Input name='email' type='email' placeholder='Enter your email' />
                 <Input name='password' type='password' placeholder='Enter your password' />
-                <Button>
+                <Button type='submit' className='w-full'>
                     Register Now
                 </Button>
             </Card>
