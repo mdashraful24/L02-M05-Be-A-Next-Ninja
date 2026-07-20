0c5a4d7903606e43043fb73186d4827ea62f71ba
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-07-21 02:22:12 +0600

    added authorizing role based protected routes in proxy

--- nextjs-press-frontend-project/proxy.ts
+++ nextjs-press-frontend-project/proxy.ts
@@ -1,14 +1,19 @@
-import jwt, { JwtPayload } from 'jsonwebtoken';
+import { JwtPayload } from 'jsonwebtoken';
 // import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { jwtUtils } from './utils/jwt';
 
 const AUTH_ROUTES = [
     "/login",
     "/register",
     "/forgot-password",
-    "/reset-password"
-]
+];
+
+const PUBLIC_ROUTES = [
+    "/",
+    "/news",
+];
 
 // This function can be marked `async` if using `await` inside
 export async function proxy(request: NextRequest) {
@@ -19,14 +24,21 @@ export async function proxy(request: NextRequest) {
 
     const accessToken = request.cookies.get("accessToken")?.value;
 
-    const decodedToken = accessToken ? jwt.decode(accessToken) as JwtPayload : null;
+    const decodedToken = accessToken ? jwtUtils.verifyToken(accessToken, process.env.JWT_ACCESS_SECRET as string) : null;
 
     let userRole = null;
 
-    if (decodedToken) {
-        userRole = decodedToken.role;
+    if(!decodedToken?.success){
+        // Token has expired or invalid, clear the cookies
+        cookieStore.delete("accessToken");
+        return NextResponse.redirect(new URL("/login", request.url));
     }
 
+    if (decodedToken?.success && decodedToken.data) {
+        userRole = (decodedToken.data as JwtPayload).role;
+    }
+
+    // If access token is present and trying to access auth routes then redirect to dashboard based on role
     if (accessToken && AUTH_ROUTES.includes(pathName)) {
         if (userRole === "USER") {
             return NextResponse.redirect(new URL('/dashboard', request.url));
@@ -39,8 +51,25 @@ export async function proxy(request: NextRequest) {
         }
     }
 
-    // return NextResponse.redirect(new URL('/', request.url))
+    // const isPublic = PUBLIC_ROUTES.some((route) => pathName === route || pathName.startsWith(`${route}/`));
+    const isPublicRoute = PUBLIC_ROUTES.some((route) => pathName === route || pathName.startsWith(route + "/"));
+    const isAuthRoute = AUTH_ROUTES.some((route) => pathName === route || pathName.startsWith(route + "/"));
 
+    // Authenticated pages protection : Authorization is not handled yet
+    if (!accessToken && !isPublicRoute && !isAuthRoute) {
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    // Authorization : Role based access control (RBAC) for authenticated users 
+    if (pathName.startsWith("/dashboard") && userRole !== "USER") {
+        return NextResponse.redirect(new URL('/not-found', request.url));
+    } else if (pathName.startsWith("/author-dashboard") && userRole !== "AUTHOR") {
+        return NextResponse.redirect(new URL('/not-found', request.url));
+    } else if (pathName.startsWith("/admin-dashboard") && userRole !== "ADMIN") {
+        return NextResponse.redirect(new URL('/not-found', request.url));
+    }
+
+    // return NextResponse.redirect(new URL('/', request.url))
     return NextResponse.next()
 }
 
